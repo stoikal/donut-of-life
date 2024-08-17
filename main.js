cac3349422743e65cc128ddc0d70a8b8cd068639
Laksito Pamilih <laksito@protonmail.com>
2024-08-17 19:58:37 +0700

    map game of life on donut

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import GameOfLife from './life';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -7,25 +8,27 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-function createCheckerboardTexture(w = 80, h = 16, flip = false) {
-  const cellSize = 10
+function createMap(gameOfLife) {
+  const cellSize = 8
 
   const canvas = document.createElement('canvas');
-  canvas.width = w * cellSize;
-  canvas.height = h * cellSize;
+  canvas.width = gameOfLife.w * cellSize
+  canvas.height = gameOfLife.h * cellSize
   const context = canvas.getContext('2d');
 
-  
-  for (let x = 0; x < canvas.width; x += cellSize) {
-    for (let y = 0; y < canvas.height; y += cellSize) {
-      if ((x + y) % (2 * cellSize) === 0) { // even cell
-        context.fillStyle = flip ? 'green' : 'red';
-      } else {
-        context.fillStyle = flip ? 'black' : 'black';
+  gameOfLife.state.forEach((row, rowIndex) => {
+    row.forEach((col, colIndex) => {
+      const x = colIndex * cellSize
+      const y = rowIndex * cellSize
+
+      if (col) {
+        context.fillStyle = '#00ffff';
+        context.fillRect(x, y, cellSize, cellSize);
       }
-      context.fillRect(x, y, cellSize, cellSize);
-    }
-  }
+    })
+  });
+
+
 
   const texture = new THREE.Texture(canvas);
   texture.needsUpdate = true;
@@ -35,41 +38,37 @@ function createCheckerboardTexture(w = 80, h = 16, flip = false) {
 const d = {
   torusRadius: 10,
   tubeRadius: 4,
-  radialSegments: 20,
-  tubularSegments: 80,
+  radialSegments: 40,
+  tubularSegments: 160,
 }
 
+const gameOfLife = new GameOfLife(d.tubularSegments, d.radialSegments)
+
 const geometry = new THREE.TorusGeometry(d.torusRadius, d.tubeRadius, d.radialSegments, d.tubularSegments ); 
-const material = new THREE.MeshBasicMaterial({ map: createCheckerboardTexture(d.tubularSegments, d.radialSegments) }); 
-// const material = new THREE.MeshToonMaterial( { color: 0x00ff00 } ); 
+const material = new THREE.MeshBasicMaterial({ map: createMap(gameOfLife) }); 
 
 const torus = new THREE.Mesh( geometry, material );
 
-// const wireframe = new THREE.WireframeGeometry( geometry );
-
-// const torus = new THREE.LineSegments( wireframe );
 torus.material.depthTest = false;
-torus.material.opacity = 0.5;
 torus.material.transparent = true;
 
 scene.add( torus );
 
 camera.position.z = 40;
 
-let flip = false
-const MAX_FPS = 4
+const MAX_FPS = 8
 const interval = 1000 / MAX_FPS;
 let lastTimestamp = 0
 
 function animate(timestamp) {
   renderer.render( scene, camera );
 
-  torus.rotation.x += .01;
-  torus.rotation.y += .01;
+  torus.rotation.x += .005;
+  torus.rotation.y += .005;
 
   if (timestamp - lastTimestamp >= interval) {
-    flip = !flip
-    torus.material.map = createCheckerboardTexture(d.tubularSegments, d.radialSegments, flip)
+    gameOfLife.next()
+    torus.material.map = createMap(gameOfLife)
     lastTimestamp = timestamp;
   }
 }
